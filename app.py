28685a54ea752e88ab85c42f7d07f86035571868
Zimani-Z <113458804+Zimani-Z@users.noreply.github.com>
2024-12-12 21:30:37 -0500

    Add file

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from your_calculator_modoule import predict_game_outcome
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    team1_stats = {
+        'points_scored_last_10': data['team1']['points'],
+        'wins': data['team1']['record'][0],
+        'losses': data['team1']['record'][1],
+        'is_home': data['team1']['is_home']
+    }
+    team2_stats = {
+        'points_scored_last_10': data['team2']['points'],
+        'wins': data['team2']['record'][0],
+        'losses': data['team2']['record'][1],
+        'is_home': data['team2']['is_home']
+    }
+    result = predict_game_outcome(team1_stats, team2_stats)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
